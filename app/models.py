d20ec3ad3dd1b35a3023f374fdc68bc9bd1d23d7
codertarasvaskiv <codertarasvaskiv@gmail.com>
2017-02-14 17:27:49 +0200

    second commit

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Number(models.Model):
+    number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return str(self.number)
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def __eq__(self, other):
+        return super().__eq__(other)
+
+
+class TimeDate(models.Model):
+    time = models.DateTimeField()
+    number_id = models.ForeignKey(Number, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.time)
+
+    def __hash__(self):
+        return super().__hash__()
+
+    def __eq__(self, other):
+        return super().__eq__(other)
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.db.models import Count, Max
+from .models import Number, TimeDate
+import datetime
 
-# Create your views here.
+
+def index(request):
+    # number = Number.objects.get(number=3)
+    # timed = TimeDate()
+    # timed.time = timezone.now()
+    # timed.number_id = number
+    # timed.save()
+    #
+    # numbers = Number.objects.all()
+    # for num in numbers:
+    #     print(num)
+
+    number = Number.objects.annotate(max_time=Max('timedate')).order_by('max_time').reverse()
+    for n in number:
+        print(n, n.timedate_set.count())
+        print(list(n.timedate_set.all())[-1])
+
+
+
+
+
+    numbers = Number.objects.all()
+    times = TimeDate.objects.all()
+    print('numbers' + str(numbers.count()))
+    print('times' + str(times.count()))
+
+
+
+    return render(request, 'index.html')
